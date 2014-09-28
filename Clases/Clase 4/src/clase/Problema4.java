fb667f0400778d316922ac40f080ec848b06ca8b
kendaltuanis <kendaltuanis@hotmail.com>
2014-09-28 12:28:23 -0600

    Clase4(ulti)

--- Clases/Clase 4/src/clase/Problema4.java
+++ Clases/Clase 4/src/clase/Problema4.java
@@ -20,32 +20,16 @@ public class Problema4 {
         return h;
     }
 
-    public void setH(int h) {
-        this.h = h;
-    }
-
     public int getJ() {
         return j;
-    }
-
-    public void setJ(int j) {
-        this.j = j;
     }
 
     public int getK() {
         return k;
     }
 
-    public void setK(int k) {
-        this.k = k;
-    }
-
     public int getL() {
         return l;
-    }
-
-    public void setL(int l) {
-        this.l = l;
     }
 
 }
