875745103307b8efba5f01f3782c7e5d4d0d09b5
kendaltuanis <kendaltuanis@hotmail.com>
2014-09-28 00:30:57 -0600

    Clase 4 (casa3)

--- Clases/Clase 4/src/clase/Problema4.java
+++ Clases/Clase 4/src/clase/Problema4.java
@@ -4,32 +4,55 @@ package clase;
 
 public class Problema4 {
 
-    public Problema4() {
+    private int h;
+    private int j;
+    private int k;
+    private int l;
+            
+            
+            
+    public Problema4(int h, int j, int k, int l) {
         
+        this.j=k;
+        this.k=h;
+        this.h=l;
+        this.l=j;
         
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
     }
     
-    int H,A,B,C,D;
-
-     public void setH(int H) {
-        this.B = H;
-    }
     
-    public void setA(int A) {
-        this.D = A;
-    }
-
-    public void setB(int B) {
-        this.C = B;
-    }
-
-    public void setC(int C) {
-        this.A = C;
-    }
-
-    public void setD(int D) {
-        this.D = D;
-    }
 
    
 
