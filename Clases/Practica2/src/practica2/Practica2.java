118a26c5108d9e2accca13a5175fb8b5fed65201
kendaltuanis <kendaltuanis@hotmail.com>
2014-10-08 16:30:55 -0600

    6casa

--- Clases/Practica2/src/practica2/Practica2.java
+++ Clases/Practica2/src/practica2/Practica2.java
@@ -1,4 +1,3 @@
-
 package practica2;
 
 import java.util.Scanner;
@@ -9,18 +8,22 @@ import java.util.Scanner;
  */
 public class Practica2 {
 
-    
     public static void main(String[] args) {
-      Scanner teclado = new Scanner(System.in);
-      
-      Problema1 problema1 = new Problema1();
-      
-      int A;
-      String hola;
-      
-      int opcion;
+        Scanner teclado = new Scanner(System.in);
+
+        Problema1 problema1 = new Problema1();
+        Problema2 problema2 = new Problema2();
+        Problema3 problema3 = new Problema3();
+        Problema4 problema4 = new Problema4();
+        Problema5 problema5 = new Problema5();
+
+        int A;
+        String hola;
+
+        int opcion;
+        char I = ' ';
         char continuar = ' ';
-       
+
         do {
             System.out.println("Digite la operación a evaluar"
                     + "\n 1."
@@ -37,44 +40,46 @@ public class Practica2 {
                 case 1:
 
                     System.out.println("Ingresar un número entre 1 y 12 ");
-                    A=teclado.nextInt(1);
-                    System.out.println(" El mes es "+ problema1.Meses(A));
-  
+                    A = teclado.nextInt(1);
+                    System.out.println(" El mes es " + problema1.Meses(A));
+
                     break;
                 case 2:
-                    System.out.println("ingrese alguna cadena");
-                    hola=teclado.nextLine();
-                    
-                    A= hola.length()-1;
+                    System.out.println("Ingrese un número ");
+                    int i = teclado.nextInt();
 
-                    System.out.println("Es "+ hola.length());
-                    
+                    System.out.println("El resultado es " + problema2.nose(i));
 
-                   
                     break;
                 case 3:
 
-                    System.out.println("Ingrese 2 números para multiplicación");
+                    System.out.println("Ingrese el estado de la nota");
 
-                   
+                    I = teclado.next().charAt(0);
+
+                    System.out.println(problema3.Revisado(I));
 
                     break;
                 case 4:
 
-                    System.out.println("Ingrese 2 números para la División");
+                    System.out.println("Ingrese el año y el mes");
+                    problema4.setYear(teclado.nextInt());
+                    int mes = teclado.nextInt();
+
+                    System.out.println(problema4.mes(mes));
 
                     break;
 
                 case 5:
 
-                    System.out.println("Ingrese 1 número para saber la raíz");
-
-                   
-
+                    System.out.println(problema5.aleatorios());
+                    
+                    break;
                 case 6:
                     System.out.println("Ingrese 2 números para la potencia");
+                    hola=teclado.next();
+                    
 
-               
                 default:
                     System.out.println("Opción Incorrecta");
 
@@ -85,7 +90,6 @@ public class Practica2 {
 
         } while ((continuar == 's') || (continuar == 'S'));
 
-        
     }
-    
+
 }
---
+++ Clases/Practica2/src/practica2/Problema6.java
@@ -0,0 +1,13 @@
+
+
+package practica2;
+
+
+public class Problema6 {
+    
+  
+    
+    
+    
+    
+}
